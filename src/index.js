d8dd83907ac22b7c1a2b6e54c584361b123b979e
dimitarkolev <dk@almero.bg>
2022-01-19 12:23:14 +0000

    Update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createMikser } from './lib/mikser'
+import { useWhiteboxFiles } from "./stores/files"
+import { useWhiteboxDocuments } from "./stores/documents"
+import { useWhiteboxConfig } from "./stores/routes"
+
+export {
+    createMikser,
+    useWhiteboxFiles,
+    useWhiteboxDocuments,
+    useWhiteboxConfig
+}
