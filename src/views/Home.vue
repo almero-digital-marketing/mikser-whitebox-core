baa7bb92929d0f5bdec03ed01e0865320a78063e
dimitarkolev <dk@almero.bg>
2022-03-02 16:32:06 +0000

    Test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
         <h2>HOME</h2>
-        <p>Projects count: {{ $collections.projects.loaded }} {{ $collections.projects.length }}</p>
+        <p>Projects: <strong>{{ $collections.projects.loaded }}</strong> Count: <strong>{{ $collections.projects.length }}</strong></p>
     </div>
 </template>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="projects">
         <h2>PROJECTS</h2>
-        <p>Projects count: {{ $collections.projects.loaded }} {{ $collections.projects.length }}</p>
+        <p>Projects: <strong>{{ $collections.projects.loaded }}</strong> Count: <strong>{{ $collections.projects.length }}</strong></p>
     </div>
 </template>
