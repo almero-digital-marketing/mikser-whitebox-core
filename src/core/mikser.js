67a8aa8c1aa5009fe6224d6ae38f5ab41520019a
dimitarkolev <dk@almero.bg>
2026-05-29 12:24:52 +0300

    Bump version to 8.0.0 and enhance data fetching in mikser.js

--- src/core/mikser.js
+++ src/core/mikser.js
@@ -1,24 +1,48 @@
 export default class {
-    constructor({ baseUrl = '', documentName = 'document', contextName = 'context' } = {}) {
+    constructor({ baseUrl = '', catlogName = "mikser", entitiesName, documentName = 'document', contextName = 'context', lang } = {}) {
         this.baseUrl = baseUrl
         this.documentName = documentName
         this.contextName = contextName
+        this.lang = lang
+        this.catlogName = catlogName
+        this.entitiesName = entitiesName
     }
     async loadSitemap() {
-        const response = await fetch(`${this.baseUrl}/data/mikser.json`)
+        const response = await fetch(`${this.baseUrl}/data/${this.catlogName}.json`)
         const documents = await response.json()
-        return documents
+        return documents.map(document => {
+            let documentBase = ''
+            if (this.lang) documentBase += `${this.lang}`
+            if (this.entitiesName) documentBase += `/${this.entitiesName}`
+            return {
+                ...document,
+                refId: documentBase ? document.refId.replace(`/${documentBase}`, '') : document.refId,
+                name: documentBase ? document.name.replace(documentBase, '') : document.name,
+            }
+        })
     }
     loadDocuments(refIds) {
         return Promise.all(refIds.map(refId => {
             if (refId == '/') refId = '/index'
-            return fetch(`${this.baseUrl}/data/${refId}${this.documentName ? '.' + this.documentName : ''}.json`)
+
+            let documentRoot = `${this.baseUrl}`
+            if (this.lang) documentRoot += `/${this.lang}`
+            if (this.entitiesName) documentRoot += `/${this.entitiesName}`
+            documentRoot += `/data/${refId}${this.documentName ? '.' + this.documentName : ''}.json`
+            
+            return fetch(documentRoot)
             .then(responese => responese.json())
         }))
     }
     loadContext(refId) {
         if (refId == '/') refId = '/index'
-        return fetch(`${this.baseUrl}/data/${refId}.${this.contextName}.json`)
+
+        let contextRoot = `${this.baseUrl}`
+        if (this.lang) contextRoot += `/${this.lang}`
+        if (this.entitiesName) contextRoot += `/${this.entitiesName}`
+        contextRoot += `/data/${refId}.${this.contextName}.json`
+
+        return fetch(contextRoot)
         .then(responese => responese.json())
     }
     async loadDocumentsByQuery(query) {
